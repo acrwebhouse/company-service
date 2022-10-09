b81eccc162c005583ca8f54db2589c97d19c025e
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-10-09 22:58:25 +0800

    getPersonalEmployeesInfo

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const serverPort = config.serverPort;
 const server = require('http').createServer(app);
 const serverUse=require('./lib/serverUse');
 const companyRestApi = require("./lib/rest_api/company");
-// const notificationRestApi = require("./lib/rest_api/notification");
+const employeesRestApi = require("./lib/rest_api/employees");
 
 console.log('config',config)
 
 serverUse.on(app);
 companyRestApi.on(app);
-// notificationRestApi.on(app);
+employeesRestApi.on(app);
 
 server.listen(serverPort);
 console.log("現在使用" + serverPort + "port");
