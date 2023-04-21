db5c88a68ae4da400263b17c17ede0b8941a50ab
chengyen.chiang <chengyen.chiang@leediancn.com>
2023-04-22 05:39:04 +0800

    create rest api url

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const server = require('http').createServer(app);
 const serverUse=require('./lib/serverUse');
 const companyRestApi = require("./lib/rest_api/company");
 const employeesRestApi = require("./lib/rest_api/employees");
+const houseRestApi = require("./lib/rest_api/house");
 
 console.log('config',config)
 
 serverUse.on(app);
 companyRestApi.on(app);
 employeesRestApi.on(app);
+houseRestApi.on(app);
 
 server.listen(serverPort);
 console.log("現在使用" + serverPort + "port");
